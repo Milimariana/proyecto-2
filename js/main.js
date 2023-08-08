44c29d0d2cc1721be0cef13af5502763a9f5361e
mildred <mili.mariana69@gmail.com>
2023-08-08 18:09:23 -0300

    mi primer comentario proyecto2

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const tituloH1 = document.querySelector("H1");
+tituloH1.innerText =  "Mildred Chacón";
+
+
+
+
+
+
+
+const menuBoton = document.querySelector("#menu-bton");
+menuBoton.addEventListener("click", function(){
+    document.body.classList.toggle("mobile-menu-active")
+
+})
+
+
+
+/* para cambiar de fondo **/
+
+let toggle=document.getElementById("toggle");
+let label_toggle=document.getElementById("label_toggle");
+toggle.addEventListener("change", (evento)=>{
+ let checked=evento.target.checked;
+ document.body.classList.toggle("dark");
+ 
+})
+
+
+function miFuncion () {
+
+  var elemento =  document.getElementById("imagen");
+  elemento.src = "img\fotomiagmoi.png"
+  
+}
+
